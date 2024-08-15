0dfe706d95d187a2cf0edd7bc2b6d0f6b0d74f57
Mazen Yasser <mazenyasser225@gmail.com>
2024-08-15 13:20:53 +0300

    Added Online status websocket and consumer

--- chats/routing.py
+++ chats/routing.py
@@ -1,7 +1,8 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
-from chats.consumers import ChatConsumer
+from chats.consumers import ChatConsumer, StatusConsumer
 
 websocket_urlpatterns = [
-    re_path(r"chat/(?P<user_name>\w+)/$", ChatConsumer.as_asgi(), name="chat")
+    re_path(r"chat/(?P<user_name>\w+)/$", ChatConsumer.as_asgi(), name="chat"),
+    re_path(r"status/$", StatusConsumer.as_asgi(), name="status"),
 ]
